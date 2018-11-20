7b6b9a3e809d529051f1cd350ca7bbea90327988
nbledsoe <42789508+nbledsoe@users.noreply.github.com>
2018-11-20 15:28:42 -0600

    Create more.js

---
+++ js/more.js
@@ -0,0 +1,10 @@
+<script>
+function myFunction() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "img_pulpit.jpg");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    x.setAttribute("alt", "The Pulpit Rock");
+    document.body.appendChild(x);
+}
+</script>
