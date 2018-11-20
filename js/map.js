5010a5e71c81ccb1d6e20d3b63ba67d73d5e5cdb
nbledsoe <42789508+nbledsoe@users.noreply.github.com>
2018-11-20 16:26:59 -0600

    creating a map

--- js/map.js
+++ js/map.js
@@ -1,1 +1,24 @@
+function initMap(){
+  var el = document.getElementById('map');
+  var iit = new google.maps.LatLng(41.8349,87.6270);
+  var crete = new google.maps.LatLng(41.4445,87.6314);
+//map setting parameters
+  var mapOptions = {
+    center: new google.maps.LatLng(41.6573,87.6800),
+    zoom: 12,
+  };
+  var myMap = new google.maps.Map(el, mapOptions);
 
+  var marker = new google.maps.Marker({
+    position: iit,
+    map: myMap,
+    animation:google.maps.Animation.BOUNCE,
+  });
+
+  var marker1 = new google.maps.Marker({
+    position: crete,
+    map: myMap,
+    animation:google.maps.Animation.BOUNCE,
+  });
+
+}
