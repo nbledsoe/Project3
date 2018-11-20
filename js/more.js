f38978f2032e5be5625b51f6430f1790561f9318
nbledsoe <42789508+nbledsoe@users.noreply.github.com>
2018-11-20 15:30:34 -0600

    Update more.js

--- js/more.js
+++ js/more.js
@@ -1,7 +1,7 @@
 <script>
 function myFunction() {
     var x = document.createElement("IMG");
-    x.setAttribute("src", "img_pulpit.jpg");
+    x.setAttribute("src", "esports.jpg");
     x.setAttribute("width", "304");
     x.setAttribute("height", "228");
     x.setAttribute("alt", "The Pulpit Rock");
