e4d497169384ecd6020dae3aa3a6af777e090b03
nbledsoe <42789508+nbledsoe@users.noreply.github.com>
2018-11-20 17:22:29 -0600

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,16 +1,23 @@
 function initMap(){
   var el = document.getElementById('map');
-  var crete = new google.maps.LatLng(41.4445,87.6314);
+  var qibao = new google.maps.LatLng(31.157839,121.361133);
+  var chenghuang = new google.maps.LatLng(31.231994,121.498188)
 //map setting parameters
   var mapOptions = {
-    center: crete,
-    zoom: 8,
+    center: new google.maps.LatLng(31.192209,121.442245),
+    zoom: 12,
   };
   var myMap = new google.maps.Map(el, mapOptions);
 
+  var marker = new google.maps.Marker({
+    position: qibao,
+    map: myMap,
+    animation:google.maps.Animation.BOUNCE,
+  });
+
   var marker1 = new google.maps.Marker({
-    position: crete;
-    map: myMap;
+    position: chenghuang,
+    map: myMap,
     animation:google.maps.Animation.BOUNCE,
   });
 
