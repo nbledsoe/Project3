43a0ec29b587307098a55e37167b2e396ff33abe
nbledsoe <42789508+nbledsoe@users.noreply.github.com>
2018-11-20 17:21:00 -0600

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,19 +1,12 @@
 function initMap(){
   var el = document.getElementById('map');
-  var iit = new google.maps.LatLng(41.8349,87.6270);
   var crete = new google.maps.LatLng(41.4445,87.6314);
 //map setting parameters
   var mapOptions = {
-    center: {lat: -34.397, lng: 150.644},
+    center: crete,
     zoom: 8,
   };
   var myMap = new google.maps.Map(el, mapOptions);
-
-  var marker = new google.maps.Marker({
-    position: iit,
-    map: myMap,
-    animation:google.maps.Animation.BOUNCE,
-  });
 
   var marker1 = new google.maps.Marker({
     position: crete;
